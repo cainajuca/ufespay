baed226faed2a51f7f8be9e26443077ced2a2a60
Luma Pontes <lumapontes@outlook.com>
2021-04-16 22:44:31 -0300

    Authentication

---
+++ src/services/UserService.js
@@ -0,0 +1,17 @@
+import api from  './api';
+
+export const createUser = async (name, email, password) => {
+  return api.post('/user', { name, email, password }).then(resp => resp.data);
+};
+
+export const deleteUser = async () => {
+  return api.delete('/user').then(resp => resp.data);
+};
+
+export const updateUser = async (name, email, password) => {
+  return api.put('/user', { name, email, password }).then(resp => resp.data);
+};
+
+export const getUser = async () => {
+  return api.get('/user').then(resp => resp.data);
+};
